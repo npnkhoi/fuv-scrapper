b079f1f63e4d4c826b0a1ba3a1b95c723800589d
npnkhoi <npnkhoi@gmail.com>
2023-04-10 19:18:10 +0700

    patch

--- patch.py
+++ patch.py
@@ -4,14 +4,13 @@ import sys
 
 folder = sys.argv[1]
 file = '2022-2023 Spring 2023 Term.json'
-folder2 = folder + '_pe'
-# os.mkdir(f'logs/{folder2}')
 
 a = json.load(open(f'logs/{folder}/{file}', 'r'))
 b = json.load(open('assets/pe.json', 'r'))
+c = json.load(open('assets/sport.json', 'r'))
 
-c = a + b
+res = a + b + c
 
-print(len(a), len(b), len(c))
 
-json.dump(c, open(f'logs/{folder2}/{file}', 'w+'))
+json.dump(res, open(f'logs/{folder}/{file}', 'w+'))
+json.dump(a, open(f'logs/{folder}/{file[:-5]}_backup.json', 'w+'))
